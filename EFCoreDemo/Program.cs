042e09d597197eb55d332de5534ec254bda3dd04
Obi Oberoi <obi@eOberoi.com>
2021-07-11 19:24:57 -0400

    initial commit

---
+++ EFCoreDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EFCoreDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the EF Core 5.0 Demo!");
+            Console.ReadLine();
+        }
+    }
+}
