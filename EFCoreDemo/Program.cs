66b133cd956fb2920a26c7f610c52f031fecac34
Obi Oberoi <obi@eOberoi.com>
2021-07-12 20:44:22 -0400

    added Movie class

--- EFCoreDemo/Program.cs
+++ EFCoreDemo/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace EFCoreDemo
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the EF Core 5.0 Demo!");
+            Console.WriteLine("Welcome to the EF Core 5.0 Demo!!!");
             Console.ReadLine();
+
         }
     }
+
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Name { get; set; }
+        //public List <Genre> Genres { get; set; }
+    }
+
+   
 }
