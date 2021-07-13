d5a3c943e005cb5c73399495fdc7ddc25dc6308c
Obi Oberoi <obi@eOberoi.com>
2021-07-12 23:31:26 -0400

    refactored the code

--- EFCoreDemo/Program.cs
+++ EFCoreDemo/Program.cs
@@ -7,23 +7,19 @@ namespace EFCoreDemo
     {
         static void Main(string[] args)
         {
-            using (var ctx = new ManyToManyDbContext())
-            {
-                ctx.Database.EnsureCreated();
+            using var ctx = new ManyToManyDbContext();
+            ctx.Database.EnsureCreated();
 
-                var comedy = new Genre() { GenreId = 1, GnereName = "Comedy" };
-                var action = new Genre() { GenreId = 2, GnereName = "Action" };
-                var horror = new Genre() { GenreId = 3, GnereName = "Horror" };
-                var drama = new Genre() { GenreId = 4, GnereName = "Drama" };
+            var comedy = new Genre() { GenreId = 1, GnereName = "Comedy" };
+            var action = new Genre() { GenreId = 2, GnereName = "Action" };
+            var horror = new Genre() { GenreId = 3, GnereName = "Horror" };
+            var drama = new Genre() { GenreId = 4, GnereName = "Drama" };
 
-                ctx.AddRange(
-                    new Movie() { MovieId = 1, Name = "Shaw Shank Redemption", Genres = new List<Genre>() { action, comedy } },
-                            new Movie { MovieId = 2, Name = "Insidious", Genres = new List<Genre>() { horror } },
-                            new Movie { MovieId = 3, Name = "Forest Gump", Genres = new List<Genre>() { drama } });
-            }
-            Console.WriteLine("Welcome to the EF Core 5.0 Demo!!!");
-            Console.ReadLine();
-
+            ctx.AddRange(
+                new Movie() { MovieId = 1, Name = "Shaw Shank Redemption", Genres = new List<Genre>() { action, comedy } },
+                        new Movie { MovieId = 2, Name = "Insidious", Genres = new List<Genre>() { horror } },
+                        new Movie { MovieId = 3, Name = "Forest Gump", Genres = new List<Genre>() { drama } });
+            ctx.SaveChanges();
         }
     }
 
