27f9b9989f185401860a3c918f41243e7696bd00
Obi Oberoi <obi@eOberoi.com>
2021-07-20 22:50:11 -0400

    added a DbSet property

--- TPH/TphDbContext.cs
+++ TPH/TphDbContext.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using TPH;
+using static TPH.Program;
 
 namespace TPH
 {
     public class TphDbContext : DbContext
     {
+        public DbSet<Person> Person { get; set; }
     }
 }
