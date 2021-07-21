115cefe69f486dcc794bee24f75a57055ba413d2
Obi Oberoi <obi@eOberoi.com>
2021-07-20 22:48:29 -0400

    moved base and derived classes outside program class

--- TPH/Program.cs
+++ TPH/Program.cs
@@ -2,29 +2,29 @@
 
 namespace TPH
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
+        }                
+    }
 
-        public abstract class Person
-        {
-            public int Id { get; set; }
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-        }
+    public abstract class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
 
-        public class Teacher : Person
-        {
-            public DateTime HireDate { get; set; }
+    public class Teacher : Person
+    {
+        public DateTime HireDate { get; set; }
 
-        }
+    }
 
-        public class Student : Person
-        {
-            public DateTime EnrollmentDate { get; set; }
-        }
+    public class Student : Person
+    {
+        public DateTime EnrollmentDate { get; set; }
     }
 }
