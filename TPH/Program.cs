f78526d3c8e726dec7d42cd50f93d56e22bd5d06
Obi Oberoi <obi@eOberoi.com>
2021-07-20 22:35:28 -0400

    add files

---
+++ TPH/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TPH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public abstract class Person
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+    }
+}
