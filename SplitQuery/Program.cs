ea4623b621dbc364bf80b61901e259fbc1581614
Obi Oberoi <obi@eOberoi.com>
2021-07-21 22:32:51 -0400

    added a new project

---
+++ SplitQuery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SplitQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
