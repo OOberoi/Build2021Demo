43ee00c8ff0bd8c4270c4025908e8d3d6a66373e
Obi Oberoi <obi@eoberoi.com>
2021-07-23 19:26:07 -0400

    added a new derived class

--- TPT/Program.cs
+++ TPT/Program.cs
@@ -15,11 +15,15 @@ namespace TPT
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
     }
 
     public class Student : Person
     {
         public DateTime EnrollmentDate { get; set; }
+    }
+
+    public class Teacher : Person
+    {
+        public DateTime HireDate { get; set; }
     }
 }
