04c17ad848a61bcc73af707fbfc11430e6a6f0a3
Obi Oberoi <obi@eoberoi.com>
2021-07-31 21:53:23 -0400

    minor changes

--- TPT/Program.cs
+++ TPT/Program.cs
@@ -6,7 +6,7 @@ namespace TPT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello TPT!");
         }
     }
 
