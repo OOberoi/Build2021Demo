cd770b2112dd1c52272b82b415279e14fa694641
Obi Oberoi <obi@eoberoi.com>
2021-08-01 15:54:08 -0400

    minor change

--- TPH/Program.cs
+++ TPH/Program.cs
@@ -6,7 +6,7 @@ namespace TPH
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello TPH!");
         }                
     }
 
