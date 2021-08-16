4df5a031347deeccc88c8facaf5839169dd0cc0c
Obi Oberoi <obi@eoberoi.com>
2021-08-15 23:22:54 -0400

    inherit from dbcontext

--- RawSQL/RawSqlContext.cs
+++ RawSQL/RawSqlContext.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
 
 namespace RawSQL
 {
-    class RawSqlContext
+    public class RawSqlContext : DbContext
     {
     }
 }
