a737923f73dd67054bdee90dd78a482046396c13
Obi Oberoi <obi@eoberoi.com>
2021-08-16 22:36:54 -0400

    added props to the employee class

--- RawSQL/Program.cs
+++ RawSQL/Program.cs
@@ -16,6 +16,13 @@ namespace RawSQL
         public string Name { get; set; }
     }
     public class Employee
-    { 
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Gender { get; set; }
+        public decimal Salary { get; set; }
+        public bool IsActive { get; set; }
+        public int DepartmentId { get; set; }
     }
 }
