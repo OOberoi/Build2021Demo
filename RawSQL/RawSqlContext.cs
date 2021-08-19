f55f931f10c27563e8914271cf50f57e69da50dd
Obi Oberoi <obi@eoberoi.com>
2021-08-19 18:19:50 -0400

    added to the fluent api

--- RawSQL/RawSqlContext.cs
+++ RawSQL/RawSqlContext.cs
@@ -25,7 +25,7 @@ namespace RawSQL
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            modelBuilder.Entity<Department>().HasKey();
             base.OnModelCreating(modelBuilder);
         }
     }
