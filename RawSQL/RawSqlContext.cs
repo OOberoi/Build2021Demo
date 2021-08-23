e2e110782797c03824964f2f44757a2809c132b8
Obi Oberoi <obi@eoberoi.com>
2021-08-22 23:54:55 -0400

    minor refactor

--- RawSQL/RawSqlContext.cs
+++ RawSQL/RawSqlContext.cs
@@ -25,11 +25,10 @@ namespace RawSQL
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Department>().ToTable("Department")
             modelBuilder.Entity<Department>()
                 .HasKey(d => d.DepartmentId)
                 .IsClustered();
-                
 
             modelBuilder.Entity<Employee>().ToTable("Employee");
             modelBuilder.Entity<Employee>()
