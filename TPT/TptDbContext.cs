f874dd2d73a772b3467f24f107352e14cd866ab1
Obi Oberoi <obi@eoberoi.com>
2021-08-28 21:20:51 -0400

    adding TPT props

--- TPT/TptDbContext.cs
+++ TPT/TptDbContext.cs
@@ -16,6 +16,15 @@ namespace TPT
         {
             optionsBuilder.UseSqlServer("server=DESKTOP-9AB4882; database=demo.TPT; trusted_connection=true;");
             base.OnConfiguring(optionsBuilder);
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+                .Property<string>("FirstName");
+            base.OnModelCreating(modelBuilder);
+
         }
     }
 }
